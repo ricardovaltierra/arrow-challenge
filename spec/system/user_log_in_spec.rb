2a935caa9a66cdb93388421b386b8b324e9b4550
ricardovaltierra <ricardo_valtierra@outlook.com>
2021-07-12 06:47:51 +0000

    Adds navbar and page styling to dashboard and index views

--- spec/system/user_log_in_spec.rb
+++ spec/system/user_log_in_spec.rb
@@ -13,8 +13,9 @@ RSpec.describe 'User Log in', type: :system do
     fill_in 'user_password', with: @user.password
     click_button 'Log in'
 
-    expect(page).to have_text "Hi #{@user.name}, with mail: #{@user.email}"
-    expect(page).to have_button 'Log out'
+    expect(page).to have_text 'Signed in successfully.'
+    expect(page).to have_text "Hi #{@user.name}"
+    expect(page).to have_content @user.email
     expect(page).to have_current_path root_path
   end
 
