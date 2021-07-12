d6af5fcbdd0393f847a14c0ae527727ff9752b96
ricardovaltierra <ricardo_valtierra@outlook.com>
2021-07-12 12:20:57 +0000

    Updates Arrow model with validations and arrows_controller for :new & :create

--- app/controllers/arrow_controller.rb
+++
@@ -1,7 +0,0 @@
-class ArrowController < ApplicationController
-  before_action :authenticate_user!
-
-  def new
-    @arrow = Arrow.new
-  end
-end
---
+++ app/controllers/arrows_controller.rb
@@ -0,0 +1,29 @@
+class ArrowsController < ApplicationController
+  before_action :set_users, only: [:new, :create]
+  before_action :authenticate_user!
+
+  def new
+    @arrow = Arrow.new
+  end
+
+  def create
+    @arrow = Arrow.new(arrow_params)
+    @arrow.author_id = current_user.id
+
+    if @arrow.save
+      redirect_to root_path, notice: 'Great! Your arrow has been successfully sent!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_users
+    @users = User.all
+  end
+
+  def arrow_params
+    params.require(:arrow).permit(:owner_id, :description)
+  end
+end
