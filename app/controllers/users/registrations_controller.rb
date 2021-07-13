9cb22a48ed956526a406a6a235438684726f1589
ricardovaltierra <ricardo_valtierra@outlook.com>
2021-07-13 17:21:04 +0000

    Adds styling to edit user view with removing unused code on views & stile fixes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -20,9 +20,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  def update
-    super
-  end
+  # def update
+  #   super
+  # end
 
   # DELETE /resource
   # def destroy
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,8 @@ module ApplicationHelper
     routes_bg_green = [user_session_path,
                        new_user_registration_path,
                        new_user_password_path,
+                       edit_user_registration_path,
+                       '/users',
                        '/users/sign_in.turbo_stream']
 
     if routes_bg_green.include? request.env['PATH_INFO']
