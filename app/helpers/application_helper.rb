d30d5332e192d3d01829fdb94a756d9bed429126
ricardovaltierra <ricardo_valtierra@outlook.com>
2021-07-13 14:42:12 +0000

    Implememnts styling on sign up form and adds helper fo setting bg-color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def set_bg_color
+    routes_bg_green = [user_session_path,
+                       new_user_registration_path,
+                       new_user_password_path,
+                       '/users/sign_in.turbo_stream']
+
+    if routes_bg_green.include? request.env['PATH_INFO']
+      'bg-ic-green'
+    else 
+      'bg-secondary-light'
+    end
+  end
 end
