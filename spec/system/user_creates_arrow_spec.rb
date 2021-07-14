b94eaf6b273c3eb2b9677bf690a92123a5bb1ceb
ricardovaltierra <ricardo_valtierra@outlook.com>
2021-07-14 10:57:25 +0000

    Sets system test scenarios for user creating an arrow, with capybara config

--- spec/system/user_creates_arrow_spec.rb
+++ spec/system/user_creates_arrow_spec.rb
@@ -3,7 +3,7 @@
 require 'system_helper'
 
 RSpec.describe 'User creates arrow', type: :system do 
-  let(:description) { Faker::Lorem.paragraph }
+  
 
   before do
     @user_1 = create :user
@@ -12,6 +12,60 @@ RSpec.describe 'User creates arrow', type: :system do
     fill_in 'user_email', with: @user_1.email
     fill_in 'user_password', with: @user_1.password
     click_button 'Log in'
-    visit new_arrow_path
+    visit root_path
+    page.save_screenshot(full: true)
+    click_link 'Create arrow'
+  end
+
+  scenario 'with the required fields properly set' do
+    select @user_2.name, from: 'arrow_owner_id'
+    fill_in 'arrow_description', with: 'This is a quite long reason to give for an arrow!'
+    click_button 'send'.upcase
+    
+    expect(page).to have_text 'Great! Your arrow has been successfully sent!'
+  end
+
+  scenario 'with no partner selected from dropdown' do
+    fill_in 'arrow_description', with: 'This is a quite long reason to give for an arrow!'
+    click_button 'send'.upcase
+
+    expect(page).to have_content 'Owner must exist'
+  end
+
+  scenario 'with reason typed with less than 30 characters' do
+    select @user_2.name, from: 'arrow_owner_id'
+    fill_in 'arrow_description', with: 'This is short...'
+    click_button 'send'.upcase
+
+    expect(page).to have_content 'Description is too short (minimum is 30 characters)'
+  end
+
+  scenario 'with no reason typed on textarea' do
+    select @user_2.name, from: 'arrow_owner_id'
+    click_button 'send'.upcase
+    page.save_screenshot(full: true)
+
+    expect(page).to have_text "Description can't be blank"
+    expect(page).to have_content 'Description is too short (minimum is 30 characters)'
+  end
+
+  scenario 'with no partner selected neither reason typed' do
+    click_button 'send'.upcase
+
+    expect(page).to have_text "Description can't be blank"
+    expect(page).to have_content 'Description is too short (minimum is 30 characters)'
+    expect(page).to have_text 'Owner must exist'
+  end
+
+  scenario "but instead, declines and clicks 'CANCEL'" do
+    click_link 'cancel'.upcase
+
+    expect(page).to have_no_content 'Great! Your arrow has been successfully sent!'
+  end
+
+  scenario 'but instead, declines and clicks top-right modal corner' do
+    click_button 'Close'
+
+    expect(page).to have_no_content 'Great! Your arrow has been successfully sent!'
   end
 end
