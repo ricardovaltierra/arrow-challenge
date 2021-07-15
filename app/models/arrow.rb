6df9bd33d7cbf4f2795b0ceb91ac532a25279607
ricardovaltierra <ricardo_valtierra@outlook.com>
2021-07-15 17:34:20 +0000

    Implements arrow and user model specs

--- app/models/arrow.rb
+++ app/models/arrow.rb
@@ -2,8 +2,8 @@ class Arrow < ApplicationRecord
   validates :description, presence: true
   validates :description, length: { minimum: 30, maximum: 280 }
 
-  belongs_to :author, class_name: "User"
-  belongs_to :owner, class_name: "User"
+  belongs_to :author, class_name: 'User'
+  belongs_to :owner, class_name: 'User'
 
   scope :arrows_with_author, -> (current_user) { 
     current_user
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:user) }
+
+  it { is_expected.to be_valid }
+
+  describe 'associations' do
+    it {
+      should have_many(:authored_arrows)
+        .class_name('Arrow')
+        .dependent(:destroy)
+    }
+    it {
+      should have_many(:owned_arrows)
+        .class_name('Arrow')
+        .dependent(:destroy)
+    }
+  end
+
+  describe '#to_select' do
+    context 'when there are three users' do
+      it 'should return two users' do
+        user_1 = create :user
+        user_2 = create :user
+
+        subject.save
+
+        expect(User.to_select(subject).count).to eql 2
+      end
+
+      it 'should not return subject\'s id' do
+        user_1 = create :user
+        user_2 = create :user
+
+        subject.save
+
+        dropdown_users = User.to_select(subject)
+
+        dropdown_users.each do |user|
+          expect(user.name).not_to eq subject.name
+        end
+      end
+    end
+  end
 end
